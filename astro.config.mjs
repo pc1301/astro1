39cbc7f8d0788b5ff63356e28e5b7f8c4e1faa8e
Pradeep Chakravarthy <pc1301@gmail.com>
2024-10-02 19:01:23 +0530

    Added the adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import netlify from "@astrojs/netlify/functions";
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify(),
+  output: 'server',
+  adapter: netlify()
 });
