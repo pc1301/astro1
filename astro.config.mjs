ea07cddbfc8489e82f964af02fe82bbd56e1b746
Pradeep Chakravarthy <pc1301@gmail.com>
2024-10-03 22:59:10 +0530

    Added Endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   output: 'server',
   adapter: netlify()
 });
---
+++ src/pages/api/coffee.json.js
@@ -0,0 +1,18 @@
+// export const prerender = false;
+
+export const GET = async () => {
+
+    const res = await fetch("https://api.sampleapis.com/coffee/hot")
+    const data = await res.json()
+    
+    return new Response(
+        JSON.stringify(data),
+        // data,
+        {
+            status: 200,
+            headers: {
+              "Content-Type": "application/json"
+            }
+        }
+    )
+}
