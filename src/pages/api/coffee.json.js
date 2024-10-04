f518477bb02771a3d3f27d0d478f5eed415ac9a5
Pradeep Chakravarthy <pc1301@gmail.com>
2024-10-04 12:27:28 +0530

    Added the URL

--- src/pages/api/coffee.json.js
+++ src/pages/api/coffee.json.js
@@ -2,7 +2,7 @@
 
 export const GET = async () => {
 
-    const res = await fetch("http://api.sampleapis.com/coffee/hot")
+    const res = await fetch("https://api.sampleapis.com/coffee/hot")
     const data = await res.json()
     
     return new Response(
