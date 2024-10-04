afe274e6205fcb55f1a43a709e191fdb18a889e9
Pradeep Chakravarthy <pc1301@gmail.com>
2024-10-04 21:10:54 +0530

    Added Turso

--- src/pages/api/coffee.json.js
+++ src/pages/api/coffee.json.js
@@ -4,16 +4,16 @@ import { db, Inquiry } from "astro:db"
 
 export const GET = async () => {
 
-    // const res = await fetch("https://api.sampleapis.com/coffee/hot")
-    // const data = await res.json()
+    const res = await fetch("https://api.sampleapis.com/coffee/hot")
+    const data = await res.json()
 
-    const resInq = await db.select().from(Inquiry);
+    // const resInq = await db.select().from(Inquiry);
     // const data = await resInq.json()
 
-    console.log("resInq: " + JSON.stringify(resInq))
+    // console.log("resInq: " + JSON.stringify(resInq))
 
     return new Response(
-        JSON.stringify(resInq),
+        JSON.stringify(data),
         // resInq,
         {
             status: 200,
